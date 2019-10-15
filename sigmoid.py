827b26b8c4d5bfbe7b5459d8d8319380ac6e70d1
kian behzad <kian.behzad@gmail.com>
2019-10-15 13:50:44 +0330

    sigmoid now gets matrixes too

--- sigmoid.py
+++ sigmoid.py
@@ -1,8 +1,20 @@
 import math
 import numpy as np
 
-def sigmoid(x: float):
-    return (1 / (1 + math.exp(-x)))
+# def sigmoid(x: float):
+#     return (1 / (1 + math.exp(-x)))
+
+def sigmoid(x: np.ndarray):
+    if type(x) == np.ndarray:
+        a = np.arange(x.shape[0] * x.shape[1], dtype=np.float).reshape(x.shape[0], x.shape[1])
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                a[i][j] = sigmoid(x[i][j])
+        return a
+
+    if type(x) == np.int64 or type(x) == np.float:
+        return (1 / (1 + math.exp(-x)))
+
 
 def dSigmoid(x: float):
     #return (2*math.exp(-x)) / ((1 + math.exp(-x)) ** 2)
@@ -16,5 +28,5 @@ def sigmatrix(a: np.ndarray):
         sumDSigmoid += dSigmoid(i)
     return (sumSigmoid, sumDSigmoid)
 
-a = np.array([[1, 2, 3], [1, 2, 3]])
-print(sigmatrix(a))
+a = np.array([[1, 2, 3], [4, 5, 10]])
+print(sigmoid(a))
